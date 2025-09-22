6d6f9f39e252020aa4512a0c1606cb3d48c90f47
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2025-09-21 21:19:49 -0300

    primeiros 12 exercicios da lista dml1

---
+++ DML_LISTA1/DML1_4.sql
@@ -0,0 +1,44 @@
+USE DDL1_4;
+
+INSERT INTO Clientes (nome, email, telefone)
+VALUES 
+('Ana Souza', 'ana.souza@email.com', '11988887777'),
+('Carlos Pereira', 'carlos.pereira@email.com', '21999996666'),
+('Mariana Lima', 'mariana.lima@email.com', '11911112222');
+
+UPDATE Clientes
+SET telefone = 21999999999
+WHERE id = 2;
+
+DELETE FROM Clientes
+WHERE id = 3;
+
+
+INSERT INTO Produtos(nome,preco,categoria,estoque)
+VALUES 
+('banana',2.90,'frutas',12),
+('maçã',5.00,'frutas',1),
+('corrente dourada',0.90,'Acessórios',120),
+('berinjela',6.00,'verduras',2),
+('tomate',20,'frutas',0);
+
+UPDATE Produtos 
+SET preco = 1.200
+WHERE categoria = 'Acessórios';
+
+DELETE FROM Produtos 
+WHERE estoque = 0;
+
+
+
+INSERT INTO Pedidos (id_clientes, valor_total, status_pedido)
+VALUES
+(1, 150.50, 'CANCELADO'),
+(2, 299.90, 'PENDENTE'); 
+
+UPDATE Pedidos
+SET status_pedido = 'ENVIADO'
+WHERE status_pedido = 'PENDENTE';
+
+DELETE FROM Pedidos
+WHERE status_pedido = 'CANCELADO';
---
+++ DML_LISTA1/DML5_12.sql
@@ -0,0 +1,104 @@
+USE DDL5_12; 
+
+USE DDL5_12;
+
+
+INSERT INTO Cursos (nome, carga_horaria, modalidade, ativo)
+VALUES
+('Banco de dados', 720, 'EAD', TRUE),
+('Hacker do zero ao avançado', 20, 'EAD', TRUE),
+('Injeções SQL', 200, 'Presencial', FALSE);
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Cursos
+SET carga_horaria = 2000
+WHERE nome = 'Banco de dados';
+SET SQL_SAFE_UPDATES = 1;
+
+INSERT INTO Alunos (nome, email, curso_id)
+VALUES
+('Ana Silva', 'ana.silva@email.com', 1),
+('Carlos Souza', 'carlos.souza@email.com', 2),
+('Mariana Costa', 'mariana.costa@email.com', 1),
+('João Pereira', 'joao.pereira@email.com', 2),
+('Joana', 'joana.@email.com', 2),
+('Linda Maria de Assis', 'lindinha@email.com', 1),
+('Felipe Spec', 'felipinho@email.com', 2);
+
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Alunos
+SET email = 'felipinhoSpec@gmail.com'
+WHERE id = 7;
+SET SQL_SAFE_UPDATES = 1;
+
+UPDATE Cursos
+SET ativo = FALSE
+WHERE id = 1;
+
+DELETE FROM Alunos
+WHERE curso_id IN (SELECT id FROM (SELECT id FROM Cursos WHERE ativo = FALSE) AS temp);
+
+DELETE FROM Cursos
+WHERE ativo = FALSE;
+
+INSERT INTO Notas (aluno_id, disciplina, avaliacao, nota) VALUES
+(2, 'Banco de Dados', 'Prova1', 8.5),
+(4, 'Banco de Dados', 'Prova2', 9.0),
+(5, 'Banco de Dados', 'Prova1', 7.5),
+(7, 'Banco de Dados', 'Projeto Final', 9.5);
+
+
+UPDATE Notas 
+SET nota = 2.0
+WHERE avaliacao = 'Prova2'; 
+
+DELETE FROM Notas
+WHERE nota < 3;
+
+INSERT INTO Departamentos (codigo, nome, centro_custo, ativo) VALUES
+('DEP01', 'TI', 'CC101', TRUE),
+('DEP02', 'RH', 'CC202', TRUE),
+('DEP03', 'FIN', 'CC303', TRUE),
+('DEP04', 'Financeiro', 'CC404', FALSE);
+
+UPDATE Departamentos 
+SET nome = 'Financeiro Geral'
+WHERE nome = 'FIN';
+
+DELETE FROM Departamentos 
+WHERE ativo = FALSE;
+
+INSERT INTO Editoras (nome, cnpj, cidade, uf) VALUES
+('Editora Ciência Viva', '12345678000199', 'São Paulo', 'SP'),
+('Editora Alpha', '98765432000155', 'Rio de Janeiro', 'RJ');
+
+INSERT INTO Livros (id_editora,isbn, titulo, autor, ano) VALUES
+(2,'9494359320494', 'Dom Casmurro', 'Machado de Assis', 2025),
+(2,'9788525061643', 'O Alquimista', 'Paulo Coelho', 1988),
+(2,'9780451524935', '1984', 'George Orwell', 1949),
+(2,'9404208450820', 'Lisístrata', 'Lisístrata', 411),
+(2,'9780550603358', 'Verity', 'Colleen Hoover', 2018);
+
+UPDATE Livros
+SET ano = 1899
+WHERE isbn = '9788535902778';
+
+DELETE FROM Livros
+WHERE isbn = '9780451524935'; 
+
+DELETE FROM Editoras
+WHERE id NOT IN (
+    SELECT id_editora FROM (SELECT id_editora FROM Livros) AS temp
+);
+
+INSERT INTO Autores (nome, nacionalidade, email, data_nascimento) 
+VALUES
+('Machado de Assis', 'Brasileiro', 'machado@gmail.com', '1839-06-21'),
+('Colleen Hoover', 'Estados Unidos', 'colleen@gmail.com', '1979-12-11'),
+('Thalita Rebouças', 'Brasileira', 'thalita@gmail.com', '1974-11-10');
+
+DELETE FROM Autores
+WHERE nome NOT IN (
+    SELECT autor FROM (SELECT autor FROM Livros) AS temp
+);
